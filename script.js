be3a52346220e4c52a6c3d571aaa6289c9fd5ebb
alexraduca01 <alex.raduca01@icloud.com>
2023-11-07 14:38:12 +0100

    completed

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const { createApp } = Vue;
 createApp({
     data() {
       return {
-        
+        emailList: [],
       }
     },
     methods: {
-
+        
+    },
+    mounted() {
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((emails) => {
+                this.emailList.push(emails.data.response);
+            });
+        };
     },
 }).mount('#app')
